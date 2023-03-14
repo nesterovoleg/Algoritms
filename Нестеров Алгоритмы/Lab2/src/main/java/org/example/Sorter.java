b61aef8ca18662af9c2b6ca91c64801a2580a74c
nesterovoleg <127876451+nesterovoleg@users.noreply.github.com>
2023-03-14 23:44:55 +0200

    Delete Нестеров Алгоритмы directory

--- Нестеров Алгоритмы/Lab2/src/main/java/org/example/Sorter.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import java.util.ArrayList;
-
-public interface Sorter {
-    ArrayList<Integer> sort(ArrayList<Integer> input);
-}
--- Нестеров Алгоритмы/Lab3/src/SumNoUsingPlus.java
+++
@@ -1,7 +0,0 @@
-public class SumNoUsingPlus {
-    public int execute(int a, int b){
-        if(b==0)
-            return a;
-        return  execute(++a,--b);
-    }
-}
